73affcf60cc6a38177facff847f70a10b242e176
Yousuf <josephali1993@gmail.com>
2020-05-19 10:23:14 +0400

    #Repackaging
    #Rename : MainActivity to SimpleBottomNavActivity

---
+++ app/src/main/java/com/programining/bottomnavview/activities/SimpleBottomNavActivity.java
@@ -0,0 +1,16 @@
+package com.programining.bottomnavview.activities;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.programining.bottomnavview.R;
+
+public class SimpleBottomNavActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_simple_bottom_nav);
+    }
+}
