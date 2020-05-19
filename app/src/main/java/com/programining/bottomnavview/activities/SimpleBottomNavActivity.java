4e07f83f8a5aef03a408a2f5f6beb35e7b197818
Yousuf <josephali1993@gmail.com>
2020-05-19 10:49:57 +0400

    +Icons : ic_dashboard, ic_home, ic_notification
    +BottomNav

--- app/src/main/java/com/programining/bottomnavview/activities/SimpleBottomNavActivity.java
+++ app/src/main/java/com/programining/bottomnavview/activities/SimpleBottomNavActivity.java
@@ -4,13 +4,17 @@ import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.programining.bottomnavview.R;
 
 public class SimpleBottomNavActivity extends AppCompatActivity {
+
+    private BottomNavigationView mBottomNav;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_simple_bottom_nav);
+        mBottomNav = findViewById(R.id.bottom_nav);
     }
 }
