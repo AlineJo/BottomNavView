515d47fa7bd7dc1a762c377efdad4252211f49c1
Yousuf <josephali1993@gmail.com>
2020-05-19 12:34:28 +0400

    +AdvanceBottomNavActivity

---
+++ app/src/main/java/com/programining/bottomnavview/activities/AdvanceBottomNavActivity.java
@@ -0,0 +1,21 @@
+package com.programining.bottomnavview.activities;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.programining.bottomnavview.R;
+
+public class AdvanceBottomNavActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_advance_bottom_nav);
+        ViewPager viewPager = findViewById(R.id.view_pager);
+        BottomNavigationView mBottomNav = findViewById(R.id.bottom_nav);
+
+    }
+}
